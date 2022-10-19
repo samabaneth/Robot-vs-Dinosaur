1ba8e162a3c6be1f6dcc0c3ed985f9066a79676a
Samantha Baneth <sambaneth8@gmail.com>
2022-10-19 17:33:34 -0600

    gave robo and dino names

--- dinosaur.py
+++ dinosaur.py
@@ -1,2 +1,8 @@
+from statistics import correlation
+from typing_extensions import Self
+
+
 class Dinosaur:
-    
+    def __init__(self):
+        self.name = "Cora"
+        self.health = 100
--- robot.py
+++ robot.py
@@ -1,2 +1,4 @@
 class Robot:
-    
+    def __init__(self):
+        self.name = "Michael"
+        self.health = 100
